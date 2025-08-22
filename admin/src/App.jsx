293fc32a6bc92e1198217debe5563adf56f95494
Ravi Singh bais <ravisinghbais5@gmail.com>
2025-08-22 08:53:39 +0530

    AI integration and other updation

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,12 +6,14 @@ import Lists from './pages/Lists'
 import Orders from './pages/Orders'
 import Login from './pages/Login'
 import { adminDataContext } from './context/AdminContext'
+  import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   let {adminData} = useContext(adminDataContext)
   return (
 
     <>
+      <ToastContainer />
     {!adminData ? <Login/> : <>
 
       <Routes>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AuthContext from './context/authContext.jsx'
+import UserContext from './context/UserContext.jsx'
+import ShopContext from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
