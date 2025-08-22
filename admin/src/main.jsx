4c61cb832da9eeb9f95b0f9a54f06102bd35c752
Ravi-15-07-2003 <ravisinghbais5@gmail.com>
2025-08-22 09:37:54 +0530

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import AuthContext from './context/AuthContext.jsx'
+import AdminContext from './context/AdminContext.jsx'
 
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
